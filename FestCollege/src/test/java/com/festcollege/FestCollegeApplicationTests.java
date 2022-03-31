49db8ee62b7665bc53a0df67acc217d5e074aae0
Jinumol K J <jinumolkj@gmail.com>
2022-04-01 00:57:29 +0530

    First Commit

---
+++ FestCollege/src/test/java/com/festcollege/FestCollegeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.festcollege;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestCollegeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
